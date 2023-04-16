e301fb2c5e3b84d9a4da1b3d29b33b5676d5cc98
Bjørnar Prytz <bjornarprytz@gmail.com>
2023-04-16 23:50:00 +0200

    - hacky bugfix

--- src/forest/items/casette.gd
+++ src/forest/items/casette.gd
@@ -7,10 +7,12 @@ signal stopped
 
 var is_playing : bool
 
+@onready var label : RichTextLabel = $CasetteAnimation/C/Label
+
 var data : CasetteData :
 	set(val):
-		$Label.text = val.label
-		$ColorBand.modulate = val.color
+		$CasetteAnimation/C/Label.text = val.label
+		$CasetteAnimation/ColorBand.modulate = val.color
 		if (val.music and val.music.get_length() > 0):
 			var nFrames = $TapeAnimation.sprite_frames.get_frame_count("Tape") # Assumes FPS==1
 			$TapeAnimation.speed_scale = nFrames / val.music.get_length()
@@ -20,6 +22,8 @@ func _ready():
 	if not Engine.is_editor_hint():
 		AudioPlayer.switched_casette.connect(_on_casette_switch)
 		$TapeAnimation.animation_finished.connect(_on_tape_ran_out)
+	
+	$CasetteAnimation/C/Label.position = Vector2.ZERO # Hack because some UI sets the position  way outside the screen for some reason (related to the low scale 0.04)?
 	
 func _on_casette_switch(_playing_casette : CasetteData):
 	var was_playing = is_playing
--- src/forest/items/casette_ui.gd
+++ src/forest/items/casette_ui.gd
@@ -6,16 +6,12 @@ const PLAYING_SCALE : float = 1.5
 func set_data(data: CasetteData):
 	$Casette.data = data
 
-
 func _on_casette_started():
 	var tween = create_tween().set_parallel()
 	tween.tween_property($Casette, 'scale', Vector2.ONE * PLAYING_SCALE, 1.0)
 	tween.tween_property(self, 'custom_minimum_size', Vector2(90.0, 50.0) * PLAYING_SCALE, 1.0)
 
-
 func _on_casette_stopped():
 	var tween = create_tween().set_parallel()
 	tween.tween_property($Casette, 'scale', Vector2.ONE, 1.0)
 	tween.tween_property(self, 'custom_minimum_size', Vector2(90.0, 50.0), 1.0)
-	
-	
