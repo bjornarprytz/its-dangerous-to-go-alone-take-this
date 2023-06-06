7a66ce35f8646b037f865dd1b70c5aafdfcbb73e
Bjørnar Prytz <bjornarprytz@gmail.com>
2023-06-06 22:26:53 +0200

    Forest and moon set

--- src/dorull/ground-control.gd
+++ src/dorull/ground-control.gd
@@ -1,22 +1,23 @@
 extends CSGCylinder3D
 
+@export	var rotate_speed := -.05
 
 @onready var tree_spawner = preload("res://dorull/tree.tscn")
 
-const PIXEL_TO_METER_RATIO = 200
-
 func _ready() -> void:
-	for i in 69:
+	for i in 250:
 		spawn_tree()
 
 func spawn_tree():
+	print("Spawn tree")
 	var tree = tree_spawner.instantiate() as PineTree
 	
 	add_child(tree)
 
+	tree.rotate(Vector3.UP, randf_range(0, 360))
 	# get the tree to the surface
-	tree.tree_sprite.position.x = radius
-	tree.position.y = randf_range(-15, 15)
-	
-	tree.rotation_degrees.y = randf_range(0, 360)
-	tree.tree_sprite.scale = Vector3.ONE * randf_range(0.2, 0.6)
+	tree.tree_sprite.position.z = radius - 0.2
+	tree.position.y = [randf_range(-4.0, -1.9), randf_range(-.5, 2.0)].pick_random()
+
+func _physics_process(delta: float) -> void:
+	rotate(Vector3.UP, rotate_speed * delta)
